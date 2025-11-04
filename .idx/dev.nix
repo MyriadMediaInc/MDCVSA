ed2f73714c1b60db600a7b09805fdb85d8a7c428
jeff <jeff@myriadmedia.com>
2025-11-04 21:53:31 +0000

    updates

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,10 +1,44 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
-{ pkgs, ... }: {
+{ pkgs, ... }:
+
+let
+  # 1. Define the PHP package with all its extensions in one go.
+  # This is the correct Nix pattern.
+  php-with-extensions = pkgs.php82.withExtensions ({ enabled, all }: enabled ++ [
+    all.bcmath
+    all.ctype
+    all.curl
+    all.dom
+    all.exif
+    all.fileinfo
+    all.gd
+    all.gmp
+    all.iconv
+    all.intl
+    all.mbstring
+    all.mysqli
+    all.openssl
+    all.pdo
+    all.pdo_mysql
+    all.sqlite3
+    all.zip
+    all.zlib
+    all.opcache
+  ]);
+
+  # 2. Create a start script that uses this new PHP package.
+  start-script = pkgs.writeShellScriptBin "start-server" ''
+    #!${pkgs.bash}/bin/bash
+    ${php-with-extensions}/bin/php -S 0.0.0.0:$PORT -t public
+  '';
+
+in
+{
   channel = "stable-24.05";
   packages = [
-    pkgs.php81
-    pkgs.phpPackages.composer
+    # 3. List the packages to install.
+    php-with-extensions
+    pkgs.php82Packages.composer
+    start-script
   ];
   idx = {
     extensions = [ "google.gemini-cli-vscode-ide-companion" ];
@@ -12,7 +46,8 @@
       enable = true;
       previews = {
         web = {
-          command = [ "php" "-t" "public" "-S" "0.0.0.0:8000" ];
+          # 4. Use the start script for the preview.
+          command = [ "start-server" ];
           manager = "web";
         };
       };
