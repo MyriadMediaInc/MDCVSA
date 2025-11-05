aa2f2f0f1ef8a3c2f55eb0f1eb2caece644c2ba9
jeff <jeff@myriadmedia.com>
2025-11-05 02:31:40 +0000

    Database.php updates

--- src/Database.php
+++ src/Database.php
@@ -2,6 +2,9 @@
 
 namespace App;
 
+use PDO;
+use PDOException;
+
 class Database
 {
     private static $instance = null;
@@ -18,15 +21,15 @@ class Database
 
         $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
         $options = [
-            \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
-            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-            \PDO::ATTR_EMULATE_PREPARES   => false,
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
         ];
 
         try {
-            $this->connection = new \PDO($dsn, $user, $pass, $options);
-        } catch (\PDOException $e) {
-            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+            $this->connection = new PDO($dsn, $user, $pass, $options);
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
         }
     }
 
