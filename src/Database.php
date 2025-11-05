ac0819215d37cfea25839f5d68ddd641a8998012
jeff <jeff@myriadmedia.com>
2025-11-05 02:18:23 +0000

    Database.php syntax correction

--- src/Database.php
+++ src/Database.php
@@ -9,12 +9,12 @@ class Database
 
     private function __construct()
     {
-        $host = getenv(\'DB_HOST\');
-        $port = getenv(\'DB_PORT\');
-        $db   = getenv(\'DB_DATABASE\');
-        $user = getenv(\'DB_USERNAME\');
-        $pass = getenv(\'DB_PASSWORD\');
-        $charset = \'utf8mb4\';
+        $host = getenv('DB_HOST');
+        $port = getenv('DB_PORT');
+        $db   = getenv('DB_DATABASE');
+        $user = getenv('DB_USERNAME');
+        $pass = getenv('DB_PASSWORD');
+        $charset = 'utf8mb4';
 
         $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
         $options = [
