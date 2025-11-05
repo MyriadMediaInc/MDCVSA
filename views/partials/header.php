e9038f48112bf16e98d4db2e110df0a4fd4b16e2
jeff <jeff@myriadmedia.com>
2025-11-05 16:36:14 +0000

    header and sidebar updates

--- views/partials/header.php
+++ views/partials/header.php
@@ -1,29 +1,3 @@
-<?php
-// Start the session to access session variables
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-$isLoggedIn = isset($_SESSION['user_id']);
-$user = null;
-
-if ($isLoggedIn) {
-    // We need the get_user_by_id function, which is in auth.php
-    // and a database connection from bootstrap.php
-    require_once __DIR__ . '/../../src/bootstrap.php';
-    require_once __DIR__ . '/../../src/auth.php';
-
-    // Check if the get_user_by_id function exists before calling it
-    if (function_exists('get_user_by_id')) {
-        $user = get_user_by_id($db, $_SESSION['user_id']);
-    } else {
-        // Handle the error gracefully - maybe log it or show a generic error
-        // For now, we'll just ensure the user is not set
-        $user = null;
-    }
-}
-?>
-
 <!-- Navbar -->
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
@@ -38,37 +12,6 @@ if ($isLoggedIn) {
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
-        <?php if ($isLoggedIn && $user) : ?>
-            <!-- User Menu Dropdown -->
-            <li class="nav-item dropdown user-menu">
-                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-                    <span class="d-none d-md-inline">Welcome, <?= htmlspecialchars($user['first_name']) ?></span>
-                </a>
-                <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                    <!-- User image -->
-                    <li class="user-header bg-primary">
-                        <!-- You might want to add a user image column to your 'people' table -->
-                        <!-- <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image"> -->
-                        <p>
-                            <?= htmlspecialchars($user['first_name'] . ' ' . $user['last_name']) ?>
-                            <small>Member Since <?= date("M. Y", strtotime($user['created_at'])) ?></small>
-                        </p>
-                    </li>
-                    <!-- Menu Footer-->
-                    <li class="user-footer">
-                        <a href="#" class="btn btn-default btn-flat">Profile</a>
-                        <a href="logout.php" class="btn btn-default btn-flat float-right">Logout</a>
-                    </li>
-                </ul>
-            </li>
-        <?php else : ?>
-            <li class="nav-item">
-                <a class="nav-link" href="login.php">
-                    <i class="fas fa-sign-in-alt"></i> Login
-                </a>
-            </li>
-        <?php endif; ?>
-
         <li class="nav-item">
             <a class="nav-link" data-widget="fullscreen" href="#" role="button">
                 <i class="fas fa-expand-arrows-alt"></i>
--- views/partials/sidebar.php
+++ views/partials/sidebar.php
@@ -1,3 +1,22 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+$isLoggedIn = isset($_SESSION['user_id']);
+$user = null;
+
+if ($isLoggedIn) {
+    // The user object is needed. Ensure bootstrap and auth functions are available.
+    // A more advanced structure would use a service container or a session class to manage the user object.
+    require_once __DIR__ . '/../../src/bootstrap.php';
+    require_once __DIR__ . '/../../src/auth.php';
+
+    if (function_exists('get_user_by_id')) {
+        $user = get_user_by_id($db, $_SESSION['user_id']);
+    } 
+}
+?>
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="/mdcvsa/index.php" class="brand-link">
@@ -8,9 +27,19 @@
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="info">
-                <a href="#" class="d-block">Guest User</a>
-            </div>
+            <?php if ($isLoggedIn && $user): ?>
+                <div class="image">
+                    <!-- We'll use a default image for now, with standard AdminLTE classes -->
+                    <img src="https://adminlte.io/themes/v3/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                </div>
+                <div class="info">
+                    <a href="#" class="d-block"><?= htmlspecialchars($user['first_name'] . ' ' . $user['last_name']) ?></a>
+                </div>
+            <?php else: ?>
+                <div class="info">
+                    <a href="login.php" class="d-block">Guest User</a>
+                </div>
+            <?php endif; ?>
         </div>
 
         <!-- Sidebar Menu -->
@@ -22,13 +51,28 @@
                         <p>Dashboard</p>
                     </a>
                 </li>
+                <?php if (!$isLoggedIn): ?>
                 <li class="nav-item">
                     <a href="/mdcvsa/register.php" class="nav-link">
                         <i class="nav-icon fas fa-user-plus"></i>
                         <p>Register</p>
                     </a>
                 </li>
-                <!-- Add more menu items here based on user roles -->
+                <li class="nav-item">
+                    <a href="/mdcvsa/login.php" class="nav-link">
+                        <i class="nav-icon fas fa-sign-in-alt"></i>
+                        <p>Login</p>
+                    </a>
+                </li>
+                <?php endif; ?>
+                 <?php if ($isLoggedIn): ?>
+                <li class="nav-item">
+                    <a href="/mdcvsa/logout.php" class="nav-link">
+                        <i class="nav-icon fas fa-sign-out-alt"></i>
+                        <p>Logout</p>
+                    </a>
+                </li>
+                <?php endif; ?>
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
