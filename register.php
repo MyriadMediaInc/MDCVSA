048ca251b3cee2c18c92e4049f32370d63eea4c2
jeff <jeff@myriadmedia.com>
2025-11-05 03:36:36 +0000

    register.php created

---
+++ register.php
@@ -0,0 +1,11 @@
+<?php
+
+// register.php
+
+// Include the bootstrap file to get things started
+require_once __DIR__ . '/src/bootstrap.php';
+
+// The view logic will be in a separate file
+// This keeps the project structure clean
+// We will create this file in the next step
+include __DIR__ . '/views/pages/register.php';
