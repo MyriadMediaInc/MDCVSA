f5496f1c6cbdd0867a5a5f99abc188f3c0e3ae17
jeff <jeff@myriadmedia.com>
2025-11-05 16:08:24 +0000

    Header and login updates

--- views/pages/login.php
+++ views/pages/login.php
@@ -1,115 +1,50 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>MDCVSA | Log in</title>
+ 
+<div class="card-body">
+    <p class="login-box-msg">Sign in to start your session</p>
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="vendor/almasaeed2010/adminlte/plugins/fontawesome-free/css/all.min.css">
-  <!-- SweetAlert2 -->
-  <link rel="stylesheet" href="vendor/almasaeed2010/adminlte/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="vendor/almasaeed2010/adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="vendor/almasaeed2010/adminlte/dist/css/adminlte.min.css">
-</head>
-<body class="hold-transition login-page">
-<div class="login-box">
-  <div class="card card-outline card-primary">
-    <div class="card-header text-center">
-      <a href="/" class="h1"><b>MDCVSA</b></a>
-    </div>
-    <div class="card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
+    <?php if (!empty($errors)) : ?>
+        <div class="alert alert-danger">
+            <?php foreach ($errors as $error) : ?>
+                <p><?= htmlspecialchars($error) ?></p>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
 
-      <form action="login.php" method="post">
+    <form action="login.php" method="post">
         <div class="input-group mb-3">
-          <input type="email" class="form-control" name="email" placeholder="Email" required>
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
+            <input type="email" class="form-control" placeholder="Email" name="email" required>
+            <div class="input-group-append">
+                <div class="input-group-text">
+                    <span class="fas fa-envelope"></span>
+                </div>
             </div>
-          </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" name="password" placeholder="Password" required>
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
+            <input type="password" class="form-control" placeholder="Password" name="password" required>
+            <div class="input-group-append">
+                <div class="input-group-text">
+                    <span class="fas fa-lock"></span>
+                </div>
             </div>
-          </div>
         </div>
         <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember" name="remember">
-              <label for="remember">
-                Remember Me
-              </label>
+            <div class="col-8">
+                <div class="icheck-primary">
+                    <input type="checkbox" id="remember" name="remember">
+                    <label for="remember">
+                        Remember Me
+                    </label>
+                </div>
             </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
+            <!-- /.col -->
+            <div class="col-4">
+                <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+            </div>
+            <!-- /.col -->
         </div>
-      </form>
+    </form>
 
-      <p class="mb-1">
-        <a href="forgot-password.php">I forgot my password</a>
-      </p>
-      <p class="mb-0">
+    <p class="mb-0">
         <a href="register.php" class="text-center">Register a new membership</a>
-      </p>
-    </div>
-    <!-- /.card-body -->
-  </div>
-  <!-- /.card -->
+    </p>
 </div>
-<!-- /.login-box -->
-
-<!-- jQuery -->
-<script src="vendor/almasaeed2010/adminlte/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="vendor/almasaeed2010/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- SweetAlert2 -->
-<script src="vendor/almasaeed2010/adminlte/plugins/sweetalert2/sweetalert2.min.js"></script>
-<!-- AdminLTE App -->
-<script src="vendor/almasaeed2010/adminlte/dist/js/adminlte.min.js"></script>
-
-<script>
-$(function() {
-    // Check for registration success
-    <?php if (isset($_GET['registered']) && $_GET['registered'] === 'true'): ?>
-        Swal.fire({
-            icon: 'success',
-            title: 'Registration Successful!',
-            text: 'You can now log in with your credentials.',
-            confirmButtonText: 'OK'
-        });
-    <?php endif; ?>
-
-    // Check for standard PHP error messages
-    <?php if (!empty($errors)):
-        $errorHtml = '<ul class="text-left">';
-        foreach ($errors as $error) {
-            $errorHtml .= '<li>' . htmlspecialchars($error) . '</li>';
-        }
-        $errorHtml .= '</ul>';
-    ?>
-        Swal.fire({
-            icon: 'error',
-            title: 'Login Failed',
-            html: '<?= $errorHtml ?>',
-            confirmButtonText: 'Try Again'
-        });
-    <?php endif; ?>
-});
-</script>
-
-</body>
-</html>
