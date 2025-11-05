c3a904605ffda200761b66bbb0396a86abeb4ff9
jeff <jeff@myriadmedia.com>
2025-11-05 02:14:48 +0000

    error reporting

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 // Basic bootstrap
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/bootstrap.php';
