a3166fd51ca71e917c8fdc5743e2736e268dd654
jeff <jeff@myriadmedia.com>
2025-11-06 23:45:30 +0000

    starting from scratch

--- public/admin/leagues.php
+++
@@ -1,8 +0,0 @@
-<?php
-// public/admin/leagues.php
-
-$pageTitle = 'Manage Leagues';
-$contentView = __DIR__ . '/../../../views/pages/admin/leagues_list.php';
-
-// The app.php layout will handle all bootstrapping and authentication.
-include __DIR__ . '/../../../views/layouts/app.php';
--- src/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/Database.php';
-
-// The base URL of the application.
-// This is used to create absolute URLs for assets and links.
-// It should point to the public directory.
-define('BASE_URL', 'http://13.222.190.11/mdcvsa');
